5e231f2de01cabf8ad17ec4721b15a09cb6d928f
Iggy <igoririanto@rocketmail.com>
2018-07-26 18:38:52 -0500

    add subst2

---
+++ subst2.scm
@@ -0,0 +1,8 @@
+(define subst2
+  (lambda (new o1 o2 lat)
+    (cond
+      ((null? lat) (quote()))
+      (else (cond
+             ((eq? o1 (car lat)) (cons new (cdr lat)))
+             ((eq? o2 (car lat)) (cons new (cdr lat)))
+             (else (cons (car lat) (subst2 new o1 o2 (cdr lat)))))))))
