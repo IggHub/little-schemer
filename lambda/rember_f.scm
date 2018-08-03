28cba6a2aba727ecb3a2ad0627f77b2530c70f2c
Iggy <igoririanto@rocketmail.com>
2018-08-03 13:17:44 -0500

    add remberF

---
+++ lambda/rember_f.scm
@@ -0,0 +1,8 @@
+(define rember-f
+  (lambda (test? a l)
+    (cond
+      ((null? l) (quote ()))
+      (else (cond
+              ((test? (car l) a)1 (cdr l))
+              (else (cons (car l)
+                          (rember-f test? a (cdr l)))))))))
